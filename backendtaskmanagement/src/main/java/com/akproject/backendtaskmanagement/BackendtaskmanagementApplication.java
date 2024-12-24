d70cf75bb509de2b983ed87243c8ef0c205d7ad1
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2024-12-25 05:29:09 +0530

    backend task management spring project

---
+++ backendtaskmanagement/src/main/java/com/akproject/backendtaskmanagement/BackendtaskmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.akproject.backendtaskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtaskmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtaskmanagementApplication.class, args);
+	}
+
+}
---
+++ backendtaskmanagement/src/test/java/com/akproject/backendtaskmanagement/BackendtaskmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akproject.backendtaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtaskmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
