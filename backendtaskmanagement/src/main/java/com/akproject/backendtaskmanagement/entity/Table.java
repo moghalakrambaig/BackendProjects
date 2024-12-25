ef0592a3057dc73cf96408ab5b395d860dd4901e
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2024-12-26 03:35:08 +0530

    added users entity

---
+++ backendtaskmanagement/src/main/java/com/akproject/backendtaskmanagement/entity/Table.java
@@ -0,0 +1,5 @@
+package com.akproject.backendtaskmanagement.entity;
+
+public class Table {
+
+}
---
+++ backendtaskmanagement/src/main/java/com/akproject/backendtaskmanagement/entity/Users.java
@@ -0,0 +1,27 @@
+package com.akproject.backendtaskmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"email"})
+})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+}
