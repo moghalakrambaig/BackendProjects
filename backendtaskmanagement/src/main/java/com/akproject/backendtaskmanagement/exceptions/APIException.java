505359cf24701d6983fccd83d45ab27b10e3a6d7
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-02-10 04:40:29 +0530

    updated all folders

--- backendtaskmanagement/src/main/java/com/akproject/backendtaskmanagement/exceptions/APIException.java
+++ backendtaskmanagement/src/main/java/com/akproject/backendtaskmanagement/exceptions/APIException.java
@@ -3,7 +3,7 @@ package com.akproject.backendtaskmanagement.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST )
 public class APIException extends RuntimeException {
     private String message;
 
